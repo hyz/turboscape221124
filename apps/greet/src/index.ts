856daae7a2ba1a3d3b9245ae2e73c937cafc38bf
yezert <oodx@icloud.com>
2022-11-25 23:20:11 +0800

    esbuild-script

--- apps/greet/src/index.ts
+++ apps/greet/src/index.ts
@@ -1,15 +1,19 @@
 // import { fs, tauri } from "tauri-apps-api-dist";
 import { fs, tauri } from "tauri-apps-api";
 import init, { greet } from "../pkg";
+// import "../pkg/index_bg.wasm";
 
 console.log(1, 2, 3, fs.exists, tauri.invoke, init, greet);
 
-(async function () {
-	let bytes = await fs.readBinaryFile("../pkg/index_bg.wasm");
+(async function (index_bg_wasm) {
+	let bytes = await fs.readBinaryFile(index_bg_wasm);
 	let blob = new Blob([bytes], { type: "application/wasm" });
 	let url = URL.createObjectURL(blob);
 	await init(url);
-})().then(console.log);
+	greet("lucky day");
+})("../pkg/index_bg.wasm")
+	.then(console.log)
+	.catch(console.error);
 
 // init().then(console.log);
 // invoke("greet", { name: "foo" }).then(console.log);
