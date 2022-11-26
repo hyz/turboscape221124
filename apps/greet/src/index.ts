a08e025485ddba911c6c9855f861cae554177552
yezert <oodx@icloud.com>
2022-11-26 14:27:53 +0800

    u

--- apps/greet/src/index.ts
+++ apps/greet/src/index.ts
@@ -1,19 +1,16 @@
-// import { fs, tauri } from "tauri-apps-api-dist";
-import { fs, tauri } from "tauri-apps-api";
+import { fs, tauri } from "tauri-apps-api"; //"tauri-apps-api-dist";
 import init, { greet } from "../pkg";
-// import "../pkg/index_bg.wasm";
 
+// import "../pkg/index_bg.wasm";
 // console.log(1, 2, 3, fs.exists, tauri.invoke, init, greet);
 
-(async function (index_bg_wasm) {
-	let bytes = await fs.readBinaryFile(index_bg_wasm);
-	let blob = new Blob([bytes], { type: "application/wasm" });
-	let url = URL.createObjectURL(blob);
-	await init(url);
-	greet("lucky day");
-})("apps/greet/dist/index_bg.wasm")
-	.then(console.log)
-	.catch(console.error);
+const _name__ = "greet";
 
-// init().then(console.log);
-// invoke("greet", { name: "foo" }).then(console.log);
+fs.readBinaryFile(`apps/${_name__}/dist/index_bg.wasm`)
+	.then(async (bytes) => {
+		let blob = new Blob([bytes], { type: "application/wasm" });
+		let url = URL.createObjectURL(blob);
+		await init(url);
+		greet("lucky day");
+	})
+	.catch(console.error);
